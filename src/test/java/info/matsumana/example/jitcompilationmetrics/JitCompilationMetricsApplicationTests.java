16846307654ac0b4a06a37adc5431d03f599bff0
Manabu Matsuzaki <matsumana@gmail.com>
2020-09-20 00:20:54 +0900

    first commit

---
+++ src/test/java/info/matsumana/example/jitcompilationmetrics/JitCompilationMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.matsumana.example.jitcompilationmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitCompilationMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
